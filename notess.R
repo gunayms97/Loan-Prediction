3cd161743f4bd849e29820c56dd498430faacd9b
gunayms97 <65185080+gunayms97@users.noreply.github.com>
2021-01-26 06:47:21 +0400

    Add files via upload

---
+++ notess.R
@@ -0,0 +1,3 @@
+#Dependents -a person who is financially supported by another person:
+#CoapplicantIncome -A co-applicant is an additional person considered in the underwriting and approval of a loan or other type of application.
+#Loan_Amount_Term - Term of loan in months
